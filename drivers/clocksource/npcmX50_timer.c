0528cbc4810d085765556225a18480ffa9161c71
Avi Fishman <AviFishman70@gmail.com>
2017-09-13 13:07:28 +0300

    arm: npcm: timer: and timer to device tree
    Signed-off-by: Avi Fishman <AviFishman70@gmail.com>

--- drivers/clocksource/npcmX50_timer.c
+++ drivers/clocksource/npcmX50_timer.c
@@ -1,17 +1,11 @@
-/*---------------------------------------------------------------------------------------------------------*/
-/*  Nuvoton Technology Corporation confidential                                                            */
-/*                                                                                                         */
-/*  Copyright (c) 2008 by Nuvoton Technology Corporation                                                   */
-/*  All rights reserved                                                                                    */
-/*                                                                                                         */
-/*<<<------------------------------------------------------------------------------------------------------*/
-/* File Contents:                                                                                          */
-/*   npcmX50_timer.c                                                                                       */
-/*            This file contains Kernel timer implementation                                               */
-/*  Project:                                                                                               */
-/*            Linux Kernel                                                                                 */
-/*---------------------------------------------------------------------------------------------------------*/
-
+/*
+ * Copyright (c) 2014 Nuvoton Technology corporation.
+ * Copyright 2017 Google, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation;version 2 of the License.
+ */
 
 #include <linux/kernel.h>
 #include <linux/sched.h>
@@ -20,44 +14,21 @@
 #include <linux/err.h>
 #include <linux/clk.h>
 #include <linux/io.h>
-#include <linux/leds.h>
-#include <linux/clocksource.h>
 #include <linux/clockchips.h>
 #include <linux/of_irq.h>
-#include <linux/irq.h>
-#include <linux/clk-provider.h>
-#include <linux/clkdev.h>
 #include <linux/of_address.h>
-#include <linux/clocksource.h>
-#include <linux/clk/nuvoton.h>
 
-#include <asm/mach-types.h>
-#include <asm/mach/irq.h>
-#include <asm/mach/time.h>
+/*---------------------------------------------------------------------------*/
+/* timer used by CPU for clock events                                        */
+/*---------------------------------------------------------------------------*/
 
-//--------------------------#include <mach/map.h>
-
-// #include <mach/hal.h>
-#include "npcmX50_timer.h"
-
-//#ifdef CONFIG_OF
-//static void __iomem *timer_base = NULL;
-//#define timer_base(n)     (n==0)?timer_bases[0]:((n==1)?timer_bases[1]:timer_bases[2])
-//#else
-#define NPCMX50_TMR0_BASE_ADDR                  0xF0008000      /* Timer0  module registers  */
-//#endif
-
-/*---------------------------------------------------------------------------------------------------------*/
-/* timer used by CPU for clock events                                                                      */
-/*---------------------------------------------------------------------------------------------------------*/
-//#define TIMER_CPU 0
-
-#define REG_TCSR0   (( volatile void __iomem*)(timer_base))
-#define REG_TICR0   (( volatile void __iomem*)(timer_base + 0x8))
-#define REG_TCSR1   (( volatile void __iomem*)(timer_base + 0x4))
-#define REG_TICR1   (( volatile void __iomem*)(timer_base + 0xc))
-#define REG_TDR1    (( volatile void __iomem*)(timer_base + 0x14)) 
-#define REG_TISR    (( volatile void __iomem*)(timer_base + 0x18))
+static void __iomem *timer_base;
+#define REG_TCSR0	(timer_base)
+#define REG_TICR0	(timer_base + 0x8)
+#define REG_TCSR1	(timer_base + 0x4)
+#define REG_TICR1	(timer_base + 0xc)
+#define REG_TDR1	(timer_base + 0x14)
+#define REG_TISR	(timer_base + 0x18)
 
 #define RESETINT	0x1f
 #define PERIOD		(0x01 << 27)
@@ -70,7 +41,6 @@
 
 #define	TDR_SHIFT	24
 
-static void __iomem *timer_base;
 static unsigned int timer0_load;
 
 static int npcm750_timer_oneshot(struct clock_event_device *evt)
@@ -79,9 +49,7 @@ static int npcm750_timer_oneshot(struct clock_event_device *evt)
 
 	val = __raw_readl(REG_TCSR0);
 	val &= ~(0x03 << 27);
-
 	val |= (ONESHOT | COUNTEN | INTEN | PRESCALE);
-
 	__raw_writel(val, REG_TCSR0);
 
 	return 0;
@@ -108,7 +76,6 @@ static int npcm750_clockevent_setnextevent(unsigned long evt,
 	unsigned int val;
 
 	__raw_writel(evt, REG_TICR0);
-
 	val = __raw_readl(REG_TCSR0);
 	val |= (COUNTEN | INTEN | PRESCALE);
 	__raw_writel(val, REG_TCSR0);
@@ -117,17 +84,15 @@ static int npcm750_clockevent_setnextevent(unsigned long evt,
 }
 
 static struct clock_event_device npcm750_clockevent_device = {
-	.name				= "npcm750-timer0",
-	.features			= CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT,
-	.set_next_event		= npcm750_clockevent_setnextevent,
-	.set_state_shutdown	= npcm750_timer_oneshot,
-	.set_state_periodic	= npcm750_timer_periodic,
-	.set_state_oneshot	= npcm750_timer_oneshot,
-	.tick_resume		= npcm750_timer_oneshot,
-	.rating				= 300,
+	.name		    = "npcm750-timer0",
+	.features	    = CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT,
+	.set_next_event	    = npcm750_clockevent_setnextevent,
+	.set_state_shutdown = npcm750_timer_oneshot,
+	.set_state_periodic = npcm750_timer_periodic,
+	.set_state_oneshot  = npcm750_timer_oneshot,
+	.tick_resume	    = npcm750_timer_oneshot,
+	.rating		    = 300,
 };
-
-/*IRQ handler for the timer*/
 
 static irqreturn_t npcm750_timer0_interrupt(int irq, void *dev_id)
 {
@@ -136,7 +101,7 @@ static irqreturn_t npcm750_timer0_interrupt(int irq, void *dev_id)
 	__raw_writel(0x01, REG_TISR); /* clear TIF0 */
 
 	if (evt->event_handler)
-	    evt->event_handler(evt);
+		evt->event_handler(evt);
 
 	return IRQ_HANDLED;
 }
@@ -147,16 +112,14 @@ static struct irqaction npcm750_timer0_irq = {
 	.handler	= npcm750_timer0_interrupt,
 };
 
-static void __init npcm750_clockevents_init(int irq,struct device_node *np)
+static void __init npcm750_clockevents_init(int irq, struct device_node *np)
 {
 	unsigned int rate;
-    struct clk *clk;
-	/*struct clk *clk = clk_get(NULL, "timer0");
-	BUG_ON(IS_ERR(clk));*/
+	struct clk *clk;
 
 	__raw_writel(0x00, REG_TCSR0);
 
-    clk = of_clk_get(np, 0);
+	clk = of_clk_get(np, 0);
 
 	if (IS_ERR(clk)) {
 		pr_info("Unable to get timer clock. Assuming 25Mhz input clock.\n");
@@ -167,17 +130,9 @@ static void __init npcm750_clockevents_init(int irq,struct device_node *np)
 	}
 
 	rate = rate / (PRESCALE + 1);
-
-	/*clk_enable(clk);
-	rate = clk_get_rate(clk) / (PRESCALE + 1);*/
-
-	/*rate = 25000000 / (PRESCALE + 1);*/
-
 	timer0_load = (rate / TICKS_PER_SEC);
-
 	__raw_writel(RESETINT, REG_TISR);
 	setup_irq(irq, &npcm750_timer0_irq);
-
 	npcm750_clockevent_device.cpumask = cpumask_of(0);
 
 	clockevents_config_and_register(&npcm750_clockevent_device, rate,
@@ -189,14 +144,9 @@ static void __init npcm750_clocksource_init(struct device_node *np)
 {
 	unsigned int val;
 	unsigned int rate;
-	/*struct clk *clk = clk_get(NULL, "timer1");
-
-	BUG_ON(IS_ERR(clk));*/
 
 	__raw_writel(0x00, REG_TCSR1);
 
-	/*clk_enable(clk);
-	rate = clk_get_rate(clk) / (PRESCALE + 1);*/
 	clk = of_clk_get(np, 0);
 
 	if (IS_ERR(clk)) {
@@ -223,21 +173,32 @@ static void __init npcm750_clocksource_init(struct device_node *np)
 static int __init npcm750_timer_init(struct device_node *np)
 {
 	int irq;
+	struct resource res;
+	int ret;
 
 	irq = irq_of_parse_and_map(np, 0);
 	if (!irq) {
-		pr_err("%s: No irq passed for timer via DT\n", __func__);
+		pr_err("%s : No irq passed for timer via DT\n", __func__);
 		return -1;
 	}
 
-	timer_base = ioremap(NPCMX50_TMR0_BASE_ADDR, SZ_4K);
+	ret = of_address_to_resource(np, 0, &res);
+	if (ret) {
+		pr_err("Timer of_address_to_resource fail ret %d\n", ret);
+		return -EINVAL;
+	}
 
+	timer_base = ioremap(res.start, resource_size(&res));
+	if (!timer_base) {
+		pr_err("Timer_base ioremap fail\n");
+		return -ENOMEM;
+	}
 #ifdef CONFIG_CLKSRC_MMIO
 	npcm750_clocksource_init(np);
 #endif
-	npcm750_clockevents_init(irq,np);
+	npcm750_clockevents_init(irq, np);
 
-	pr_info("npcm750_timer_init\n");
+	pr_info("%s Done\n", __func__);
 
 	return 0;
 }
